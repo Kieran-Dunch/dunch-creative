dda28ed3ab6cbc63eab73aaa87c24f27641f9688
Kieran Dunch <dunch.kieran@gmail.com>
2022-08-01 14:08:18 -0400

    styling loosely done, moving on

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,18 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['Home', 'About', 'Projects'];
 
 const NavBar = () => {
 
-
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
+      <Container maxWidth="95%">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -34,26 +30,7 @@ const NavBar = () => {
           >
             Dunch Creative
           </Typography>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: "space-around" }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: "space-evenly" }}>
             {pages.map((page) => (
               <Button
                 key={page}
@@ -62,6 +39,13 @@ const NavBar = () => {
                 {page}
               </Button>
             ))}
+          </Box>
+          <Box>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block', backgroundColor: "slateblue" }}
+            >
+              Contact me
+            </Button>
           </Box>
         </Toolbar>
       </Container>
