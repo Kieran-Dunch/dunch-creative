ba60a94034eb36c85714d731e5cfaa89716fd3c4
Kieran Dunch <dunch.kieran@gmail.com>
2022-08-01 10:38:48 -0400

    adding files, importing mui and testing

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <div>
+      <Button variant="contained" className='button'>Hello World</Button>
+    </div>
+  )
+}
