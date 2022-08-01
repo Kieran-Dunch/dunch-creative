db002d4806148383f9880754a5e0d0ac8cd99709
Kieran Dunch <dunch.kieran@gmail.com>
2022-08-01 14:35:59 -0400

    stashed and committed

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+  <header className="App-header">
+    <h1>Dunch Creative:</h1>
+    <h2>Bringing the Arts and Tech together</h2>
+  </header>
+  )
+}
