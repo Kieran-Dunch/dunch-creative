ecc7d8853441fa6a4aeb732b13a4c9f9c7ea79bb
Kieran Dunch <dunch.kieran@gmail.com>
2022-08-05 17:41:26 -0400

    finished loose footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
-import { Box } from '@mui/material'
+import { Box, Button, List, ListItemButton, ListItemIcon, ListItemText, Stack } from '@mui/material'
 import './Footer.css'
+import { Email, GitHub, LinkedIn } from '@mui/icons-material';
 
 export default function Footer() {
+
+  const pages = ['Home', 'About', 'Projects', 'Contact'];
+
   return (
     <Box
       sx={{
@@ -13,21 +17,48 @@ export default function Footer() {
       }}
     >
       <div>
-        <h2>Dunch Creative</h2>
+        <h2>Dunch Creative: Bringing the arts and tech together</h2>
+        <p><em>Copyright Dunch Creative 2022</em></p>
       </div>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: 'none', md: 'flex' },
+            justifyContent: "space-evenly",
+            alignItems: "center"
+          }}
+        >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'inline-block', backgroundColor: "blue" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
       <div>
-        <h3>Find me here:</h3>
-        <ul>
-          <li>
-            Email
-          </li>
-          <li>
-            GitHub
-          </li>
-          <li>
-            LinkedIn
-          </li>
-        </ul>
+        <h3>Find me on:</h3>
+          <List component={Stack} direction="row">
+            <ListItemButton>
+              <ListItemIcon>
+                <Email/>
+              </ListItemIcon>
+              <ListItemText primary="Email"/>
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <GitHub/>
+              </ListItemIcon>
+              <ListItemText primary="GitHub"/>
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <LinkedIn/>
+              </ListItemIcon>
+              <ListItemText primary="LinkedIn"/>
+            </ListItemButton>
+          </List>
       </div>
     </Box>
   )
