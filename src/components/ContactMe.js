3d3ee8a1c72a547d7c4700cd0fdd387ed096e5cd
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-02 12:06:20 -0400

    made progress on contact me form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,12 +1,41 @@
-import { Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 
 export default function ContactMe() {
   return (
     <>
-      <Typography>
-        Let’s see what we can create together!
-      </Typography>
-
+      <Container>
+        <Typography>
+          Let’s see what we can create together!
+        </Typography>
+        <form action="">
+          <TextField
+            label="Full Name"
+            halfWidth
+            sx={{my: 2, mx: 4}}
+          />
+          <TextField
+            label="Email"
+            halfWidth
+            sx={{my: 2, mx: 4}}
+          />
+          <TextField
+            label="Message"
+            fullWidth
+            sx={{my: 2}}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Send me an email!"
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={5}
+            sx={{my: 2}}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Container>
     </>
   )
 }
