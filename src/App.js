542ac7a51be31d12388dbe6248275d044ce2b35a
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-02 11:34:35 -0400

    finished little call to action, on to about me page'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import Header from './components/Header'
 import Footer from './components/Footer';
 import CardList from './components/CardList';
-import { Card } from '@mui/material';
+import { Button, Card, Typography } from '@mui/material';
 
 function App() {
 
@@ -12,8 +12,25 @@ function App() {
       <NavBar/>
       <Header/>
       <CardList/>
-      <Card>
-        <p>Testing</p>
+      <Card
+        variant="outlined"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          my: 5,
+          mx: 2,
+        }}
+      >
+        <Typography>
+          Whether your project involves arts and culture, tech, education
+          or outdoor adventure, I'd love to hear from you!
+        </Typography>
+        <Button
+          sx={{ my: 2, color: 'white', display: 'block', backgroundColor: "slateblue" }}
+        >
+          Contact Me
+        </Button>
       </Card>
       <Footer/>
     </div>
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -35,8 +35,8 @@ export default function CardList() {
             <Typography sx={{ fontSize: 24 }} backgroundColor="lightgray" color="text.primary" gutterBottom>
               I am a full stack web developer with a unique background as a theatre artist and producer
             </Typography>
-            <div id="test">
-            </div>
+            <Box id="test">
+            </Box>
           </CardContent>
         </Card>
         <Card variant="outlined" sx={{minWidth: "275px", height: "500px"}}>
@@ -44,8 +44,8 @@ export default function CardList() {
             <Typography sx={{ fontSize: 24 }} backgroundColor="lightgray" color="text.primary" gutterBottom>
               I bring my soft skills as a theatre producer into software development, building out-of-the-box solutions for companies and clients alike
             </Typography>
-            <div id="rocket">
-            </div>
+            <Box id="rocket">
+            </Box>
           </CardContent>
         </Card>
         <Card variant="outlined" sx={{minWidth: "275px", height: "500px"}}>
@@ -53,8 +53,8 @@ export default function CardList() {
             <Typography sx={{ fontSize: 24 }} backgroundColor="lightgray" color="text.primary" gutterBottom>
               My process begins with a conversation, and thrives in the world of creative problem sovling
             </Typography>
-            <div id="globe">
-            </div>
+            <Box id="globe">
+            </Box>
           </CardContent>
         </Card>
       </Box>
