d32f5204458cdf4cde6a0a1c0cc666bc0752c66a
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-18 16:09:19 -0400

    configured the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 const pages = ['Home', 'About', 'Projects'];
 
@@ -16,35 +17,48 @@ const NavBar = () => {
           <Typography
             variant="h6"
             noWrap
-            component="a"
-            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
             }}
           >
-            Dunch Creative
+            <Link
+              to="/"
+              style={{ color: 'white', textDecoration: 'none', }}
+            >
+              Dunch Creative
+            </Link>
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: "space-evenly" }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: 'white', display: 'block', backgroundColor: "slateblue" }}
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block', backgroundColor: "slateblue" }}
+            >
+              <Link to='/about' style={{ textDecoration: 'none', color: 'white' }}>About</Link>
+            </Button>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block', backgroundColor: "slateblue" }}
+            >
+              <Link
+                to='/projects'
+                style={{ textDecoration: 'none', color: 'white' }}
               >
-                {page}
-              </Button>
-            ))}
+                Projects
+              </Link>
+            </Button>
           </Box>
           <Box>
             <Button
               sx={{ my: 2, color: 'white', display: 'block', backgroundColor: "slateblue" }}
             >
-              Contact me
+              <Link
+                to='/contact'
+                style={{ textDecoration: 'none', color: 'white' }}
+              >
+                Contact Me
+              </Link>
             </Button>
           </Box>
         </Toolbar>
