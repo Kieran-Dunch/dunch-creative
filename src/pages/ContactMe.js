524cc171d39ef11cb93fb051723a492f5ad6f98c
Kieran Dunch <dunch.kieran@gmail.com>
2022-09-18 16:22:45 -0400

    little cleanup

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,6 +1,40 @@
+import { Button, Container, TextField, Typography } from "@mui/material";
 
 export default function ContactMe() {
   return (
-    <div>ContactMe</div>
+    <>
+      <Container>
+        <Typography>
+          Let’s see what we can create together!
+        </Typography>
+        <form action="">
+          <TextField
+            label="Full Name"
+            halfWidth
+            sx={{ my: 2, mx: 4 }}
+          />
+          <TextField
+            label="Email"
+            halfWidth
+            sx={{ my: 2, mx: 4 }}
+          />
+          <TextField
+            label="Message"
+            fullWidth
+            sx={{ my: 2 }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Send me an email!"
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={5}
+            sx={{ my: 2 }}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Container>
+    </>
   )
 }
