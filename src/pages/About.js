891ea6e180c5b7557aa017bfe60f8361076ad2b1
Kieran Dunch <dunch.kieran@gmail.com>
2022-10-01 12:40:51 -0400

    added some shit to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,24 @@
+import { Card } from "@mui/material";
+
 export default function About() {
   return (
-    <div>
-      About
-      <h1>Something else</h1>
-    </div>
+    <>
+      <h1>
+        This page will features several text boxes with three core paragraphs with photograph
+        banners and side images
+      </h1>
+      <Card>
+        <p>Kieran dunch is a developer blah blah blah</p>
+        <img src="../../public/_MG_3186.JPG" alt="Kieran at a computer" />
+      </Card>
+      <Card>
+        <p>Kieran dunch is an artist blah blah blah</p>
+        <img src="../../public/_MG_3186.JPG" alt="Woyzeck stuff" />
+      </Card>
+      <Card>
+        <p>Kieran dunch Likes to hike!</p>
+        <img src="../../public/_MG_3186.JPG" alt="Woyzeck stuff" />
+      </Card>
+    </>
   );
 }
