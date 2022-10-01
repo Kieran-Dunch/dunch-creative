70775f59ca811d19d4ab968c39783f11ae400a31
Kieran Dunch <dunch.kieran@gmail.com>
2022-10-01 08:13:40 -0400

    fixing conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
   return (
-  <header className="App-header">
-    <h1>Dunch Creative:</h1>
-    <h2>Bringing the Arts and Tech together</h2>
-  </header>
-  )
+    <header className="App-header">
+      <h1>Dunch Creative:</h1>
+      <h2>Bringing the Arts and Tech together</h2>
+    </header>
+  );
 }
