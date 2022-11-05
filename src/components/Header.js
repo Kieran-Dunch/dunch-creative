7991415060d7b693cfd5b846416ce24eebaf98c3
Kieran Dunch <dunch.kieran@gmail.com>
2022-11-05 16:12:36 -0600

    header conversion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import "./Header.css";
 
 export default function Header() {
   return (
     <header className="App-header">
-      <h1>Dunch Creative:</h1>
-      <h2>Bringing the Arts and Tech together</h2>
+      <Typography variant="h1">Dunch Creative:</Typography>
+      <Typography variant="h2">Bringing the Arts and Tech together</Typography>
     </header>
   );
 }
