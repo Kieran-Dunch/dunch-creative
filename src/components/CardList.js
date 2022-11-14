0ddec129f8570cd0e50b694df5c5dd9713c4c97c
Kieran Dunch <dunch.kieran@gmail.com>
2022-11-13 21:25:23 -0500

    cardlist styling

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,64 +1,84 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import lottie from "lottie-web";
 import { useEffect } from "react";
-import Discs from "../lotties/108322-circle-chart.json";
-import Rocket from "../lotties/116204-flying-rocket-in-the-sky.json";
-import Globe from "../lotties/69115-spinning-globe-orange.json";
+import LightBulb from "../lotties/blueLightbulb.json";
+import Rocket from "../lotties/bluerocket.json";
+import Orbit from "../lotties/blue.json";
 
 export default function CardList() {
   // reusable component, pass it attributes to be shoved through in rows of three
   // for each loop iterating through
 
   useEffect(() => {
-    lottie.loadAnimation({
-      container: document.querySelector("#discs"),
-      animationData: Discs
+    const lightbulb = lottie.loadAnimation({
+      container: document.querySelector("#lightbulb"),
+      animationData: LightBulb
     });
 
-    lottie.loadAnimation({
+    const rocket = lottie.loadAnimation({
       container: document.querySelector("#rocket"),
       animationData: Rocket
     });
 
-    lottie.loadAnimation({
-      container: document.querySelector("#globe"),
-      animationData: Globe
+    const orbit = lottie.loadAnimation({
+      container: document.querySelector("#orbit"),
+      animationData: Orbit
     });
+
+    return () => {
+      lightbulb.destroy();
+      rocket.destroy();
+      orbit.destroy();
+    };
   }, []);
 
   return (
     <Grid container spacing={3} sx={{ p: "50px" }}>
       <Grid item xs={4}>
-        <Card variant="outlined" sx={{ minWidth: "275px", height: "500px" }}>
-          <CardContent>
+        <Card variant="outlined">
+          <CardContent sx={{ height: "80vh" }}>
             <Typography variant="h6">
               I am a full stack web developer with a unique background as a theatre artist and
-              producer
+              producer.
             </Typography>
-            <Box id="discs"></Box>
+            <CardMedia id="lightbulb" style={{ margin: "75px" }}></CardMedia>
+            <CardActions>
+              <Button color="primary" variant="contained">
+                Learn more about me!
+              </Button>
+            </CardActions>
           </CardContent>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card variant="outlined" sx={{ minWidth: "275px", height: "500px" }}>
-          <CardContent>
+        <Card variant="outlined">
+          <CardContent sx={{ height: "80vh" }}>
             <Typography variant="h6">
               I bring my soft skills as a theatre producer into software development, building
-              out-of-the-box solutions for companies and clients alike
+              out-of-the-box solutions for companies and clients alike.
             </Typography>
-            <Box id="rocket"></Box>
+            <CardMedia id="rocket"></CardMedia>
+            <CardActions>
+              <Button color="primary" variant="contained">
+                See some of my projects!
+              </Button>
+            </CardActions>
           </CardContent>
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card variant="outlined" sx={{ minWidth: "275px", height: "500px" }}>
-          <CardContent>
+        <Card variant="outlined">
+          <CardContent sx={{ height: "80vh" }}>
             <Typography variant="h6">
               My process begins with a conversation, and thrives in the world of creative problem
-              solving
+              solving.
             </Typography>
-            <Box id="globe"></Box>
+            <CardMedia id="orbit"></CardMedia>
+            <CardActions>
+              <Button color="primary" variant="contained">
+                Start a conversation!
+              </Button>
+            </CardActions>
           </CardContent>
         </Card>
       </Grid>
