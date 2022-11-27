99fe37b60e7a3a1f35f499ceb877b03e17cc3e7f
Kieran Dunch <dunch.kieran@gmail.com>
2022-11-27 12:57:26 -0500

    finsihed styling for now, need to make it responsive eventually

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -11,16 +11,18 @@ import {
 import lottie from "lottie-web";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-// import LightBulb from "../lotties/blueLightbulb.json";
+import Speech from "../lotties/NavyBlue.json";
 import Rocket from "../lotties/bluerocket.json";
 import Orbit from "../lotties/blue.json";
 
+// components is not using responsive units of measurement
+
 export default function CardList() {
   useEffect(() => {
-    // const lightbulb = lottie.loadAnimation({
-    //   container: document.querySelector("#lightbulb"),
-    //   animationData: LightBulb
-    // });
+    const speech = lottie.loadAnimation({
+      container: document.querySelector("#speech"),
+      animationData: Speech
+    });
 
     const rocket = lottie.loadAnimation({
       container: document.querySelector("#rocket"),
@@ -34,10 +36,9 @@ export default function CardList() {
 
     orbit.setSpeed(0.25);
     rocket.setSpeed(0.75);
-    // lightbulb.setSpeed(0.75);
 
     return () => {
-      // lightbulb.destroy();
+      speech.destroy();
       rocket.destroy();
       orbit.destroy();
     };
@@ -46,15 +47,15 @@ export default function CardList() {
   return (
     <Grid container spacing={3} sx={{ p: "50px" }}>
       <Grid item xs={4}>
-        <Card variant="outlined" sx={{ height: "80vh" }}>
+        <Card variant="outlined" sx={{ maxHeight: "80vh" }}>
           <CardContent>
-            <Typography variant="h6" sx={{ height: "100px", mb: "10px" }}>
+            <Typography variant="h6" sx={{ height: "100px", mb: "10px", py: "20px" }}>
               I am a full stack web developer with a unique background as a theatre artist and
               producer.
             </Typography>
-            <Divider variant="middle" color="white" />
-            <CardMedia id="lightbulb" sx={{ m: "75px" }}></CardMedia>
-            <CardActions>
+            <Divider variant="middle" color="white" sx={{ mt: "2em" }} />
+            <CardMedia id="speech"></CardMedia>
+            <CardActions sx={{ pb: "2em" }}>
               <Link to="/about" style={{ textDecoration: "none", color: "white", margin: "auto" }}>
                 <Button size="large" color="primary" variant="contained" sx={{ m: "auto" }}>
                   Learn more about me!
@@ -65,13 +66,13 @@ export default function CardList() {
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card variant="outlined" sx={{ height: "80vh" }}>
+        <Card variant="outlined" sx={{ maxHeight: "80vh" }}>
           <CardContent>
-            <Typography variant="h6" sx={{ height: "100px", mb: "10px" }}>
-              I bring my soft skills as a theatre producer into software development, building
-              out-of-the-box solutions for companies and clients alike.
+            <Typography variant="h6" sx={{ height: "100px", mb: "10px", py: "20px" }}>
+              Bringing my soft skills as a theatre producer into software, I build out-of-the-box
+              solutions for my clients.
             </Typography>
-            <Divider variant="middle" color="white" />
+            <Divider variant="middle" color="white" sx={{ mt: "2em" }} />
             <CardMedia id="rocket"></CardMedia>
             <CardActions>
               <Link
@@ -86,13 +87,13 @@ export default function CardList() {
         </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card variant="outlined" sx={{ height: "80vh" }}>
+        <Card variant="outlined" sx={{ maxHeight: "80vh" }}>
           <CardContent>
-            <Typography variant="h6" sx={{ height: "100px", mb: "10px" }}>
+            <Typography variant="h6" sx={{ height: "100px", mb: "10px", py: "20px" }}>
               My process begins with a conversation, and thrives in the world of creative problem
               solving.
             </Typography>
-            <Divider variant="middle" color="white" />
+            <Divider variant="middle" color="white" sx={{ mt: "2em" }} />
             <CardMedia id="orbit"></CardMedia>
             <CardActions>
               <Link
